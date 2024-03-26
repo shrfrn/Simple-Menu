e561df361551ad41873f9986ec82d1b754241d49
shrfrn <shrfrn@gmail.com>
2024-03-26 12:41:14 +0200

    Added about page

---
+++ js/about.js
@@ -0,0 +1,56 @@
+'use strict'
+
+function onLoad() {
+	const elMain = document.querySelector('main')
+	const elLinks = document.querySelectorAll('nav a')
+	elLinks.forEach(elLink => elLink.addEventListener('mouseenter', ev => shuffleText(ev)))
+
+    addEventListener('mousemove', ev => {
+        const chars = '   ---abcdefghijklmnopqrstuvwxyz0123456789+!@%#$*&(_)+='
+        var txt = ''
+
+        for(var i = 0; i < 8000; i++){
+            txt += chars.at(getRandomInt(0, chars.length))    
+        }
+        // elMain.dataset.bgText = txt
+        document.querySelector('.bg-text').innerText = txt
+    })
+}
+
+function shuffleText({ target: elLink }) {
+
+    if(elLink.dataset.isAnimating === 'true') return
+    else elLink.dataset.isAnimating = 'true'
+
+	const chars = 'abcdefghijklmnopqrstuvwxyz0123456789+!@%#$*&(_)+='
+	const originalText = elLink.innerText
+
+	var iterationCount = 0
+
+	const id = setInterval(() => {
+		var shuffledText = ''
+
+		for (var i = 0; i < originalText.length; i++) {
+			if (i <= Math.floor(iterationCount / 3)) {
+				shuffledText += originalText.at(i)
+			} else {
+				shuffledText += chars.at(getRandomInt(0, chars.length))
+			}
+		}
+        
+        elLink.innerText = shuffledText
+        iterationCount++
+
+        if(iterationCount === originalText.length * 3) {
+            clearInterval(id)
+            elLink.dataset.isAnimating = 'false'
+        }
+
+	}, 60)
+}
+
+function getRandomInt(min, max) {
+	const minCeiled = Math.ceil(min)
+	const maxFloored = Math.floor(max)
+	return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled) // The maximum is exclusive and the minimum is inclusive
+}
--- js/simple-menu.js
+++ js/simple-menu.js
@@ -1,10 +1,23 @@
 'use strict'
 
 function onLoad() {
-    const elMain = document.querySelector('main')
-    const elLinks = document.querySelectorAll('nav a')
+	const elMain = document.querySelector('main')
+	const elLinks = document.querySelectorAll('nav a')
 
-    elLinks.forEach((elLink, idx) => elLink.addEventListener('mouseenter', () => {
-        elMain.dataset.activeIndex = idx
-    }))
+	elLinks.forEach((elLink, idx) =>
+		elLink.addEventListener('mouseenter', () => {
+			elMain.dataset.activeIndex = idx
+		})
+	)
+}
+
+function onNavigate(ev) {
+	ev.preventDefault()
+    const elLinks = document.querySelectorAll('nav a')
+    elLinks.forEach((elLink, idx) => {
+        elLink.style.animationName = 'fadeOutLeftBig'
+        elLink.style.animationDuration = '2s'
+    })
+    // window.location = ev.target.href
+    setTimeout(() => window.location = ev.target.href, 1000)
 }
