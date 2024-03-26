e661c3fa1f1abc4e0e8510f4500f212a45014495
shrfrn <shrfrn@gmail.com>
2024-03-26 13:18:28 +0200

    Add bg text animation and throttle mouse events

--- js/about.js
+++ js/about.js
@@ -4,23 +4,21 @@ function onLoad() {
 	const elMain = document.querySelector('main')
 	const elLinks = document.querySelectorAll('nav a')
 	elLinks.forEach(elLink => elLink.addEventListener('mouseenter', ev => shuffleText(ev)))
+	addEventListener('mousemove', throttle((ev) => {
+		const chars = 'abcdefghijklmnopqrstuvwxyz0123456789@%$&_'
+		var txt = ''
 
-    addEventListener('mousemove', ev => {
-        const chars = '   ---abcdefghijklmnopqrstuvwxyz0123456789+!@%#$*&(_)+='
-        var txt = ''
-
-        for(var i = 0; i < 8000; i++){
-            txt += chars.at(getRandomInt(0, chars.length))    
-        }
-        // elMain.dataset.bgText = txt
-        document.querySelector('.bg-text').innerText = txt
-    })
+		for (var i = 0; i < 8000; i++) {
+			txt += chars.at(getRandomInt(0, chars.length))
+		}
+		// elMain.dataset.bgText = txt
+		document.querySelector('.bg-text').innerText = txt
+	}, 30))
 }
 
 function shuffleText({ target: elLink }) {
-
-    if(elLink.dataset.isAnimating === 'true') return
-    else elLink.dataset.isAnimating = 'true'
+	if (elLink.dataset.isAnimating === 'true') return
+	else elLink.dataset.isAnimating = 'true'
 
 	const chars = 'abcdefghijklmnopqrstuvwxyz0123456789+!@%#$*&(_)+='
 	const originalText = elLink.innerText
@@ -37,15 +35,14 @@ function shuffleText({ target: elLink }) {
 				shuffledText += chars.at(getRandomInt(0, chars.length))
 			}
 		}
-        
-        elLink.innerText = shuffledText
-        iterationCount++
 
-        if(iterationCount === originalText.length * 3) {
-            clearInterval(id)
-            elLink.dataset.isAnimating = 'false'
-        }
+		elLink.innerText = shuffledText
+		iterationCount++
 
+		if (iterationCount === originalText.length * 3) {
+			clearInterval(id)
+			elLink.dataset.isAnimating = 'false'
+		}
 	}, 60)
 }
 
@@ -53,4 +50,16 @@ function getRandomInt(min, max) {
 	const minCeiled = Math.ceil(min)
 	const maxFloored = Math.floor(max)
 	return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled) // The maximum is exclusive and the minimum is inclusive
+}
+
+function throttle(func, limit) {
+	var inThrottle
+    
+	return (...args) => {
+        if(inThrottle) return
+        
+        func.apply(this, args)
+        inThrottle = true
+        setTimeout(() => inThrottle = false, limit)
+	}
 }
